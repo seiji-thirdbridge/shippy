8141141b5eca322d421058383eb9beb3fcf84d7d
Seiji Okamoto <seiji.okamoto@thirdbridge.com>
2018-02-15 11:38:21 +0000

    Tutorial 3a - docker-compose and mongodb

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -4,68 +4,28 @@ package main
 
 import (
 	"log"
+	"os"
 
 	micro "github.com/micro/go-micro"
 	pb "github.com/seiji-thirdbridge/shippy/consignment-service/proto/consignment"
 	vesselProto "github.com/seiji-thirdbridge/shippy/vessel-service/proto/vessel"
-	"golang.org/x/net/context"
 )
 
-type repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-type consignmentRepository struct {
-	consignments []*pb.Consignment
-}
-
-func (repo *consignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
-}
-
-func (repo *consignmentRepository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-type service struct {
-	repo         repository
-	vesselClient vesselProto.VesselServiceClient
-}
-
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
-		MaxWeight: req.Weight,
-		Capacity:  int32(len(req.Containers)),
-	})
-	if err != nil {
-		return err
-	}
-	log.Printf("Found vessel: %s\n", vesselResponse.Vessel.Name)
-
-	req.VesselId = vesselResponse.Vessel.Id
-
-	// Save the consignment
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
+const (
+	defaultHost = "localhost:27017"
+)
 
 func main() {
-	repo := &consignmentRepository{}
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = defaultHost
+	}
+
+	session, err := CreateSession(host)
+	defer session.Close()
+	if err != nil {
+		log.Panicf("Could not connect to the datastore with host %s - %v", host, err)
+	}
 
 	srv := micro.NewService(
 		micro.Name("go.micro.srv.consignment"),
@@ -76,7 +36,7 @@ func main() {
 
 	srv.Init()
 
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{session, vesselClient})
 
 	if err := srv.Run(); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
--- vessel-service/main.go
+++ vessel-service/main.go
@@ -3,54 +3,43 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
+	"log"
+	"os"
 
 	micro "github.com/micro/go-micro"
 	pb "github.com/seiji-thirdbridge/shippy/vessel-service/proto/vessel"
 )
 
-type repository interface {
-	FindAvailable(*pb.Specification) (*pb.Vessel, error)
-}
+const (
+	defaultHost = "localhost:27017"
+)
 
-type vesselRepository struct {
-	vessels []*pb.Vessel
-}
+func createDummyData(repo repository) {
+	defer repo.Close()
 
-// FindAvailable - checks a specification against a map of vessels,
-// if capacity and max weight are below a vessels capacity and max weight,
-// then return that vessel
-func (repo *vesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
-	for _, vessel := range repo.vessels {
-		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
-			return vessel, nil
-		}
-	}
-
-	return nil, errors.New("No vessel found by that spec")
-}
-
-type service struct {
-	repo repository
-}
-
-func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
-	vessel, err := s.repo.FindAvailable(req)
-	if err != nil {
-		return err
-	}
-
-	res.Vessel = vessel
-	return nil
-}
-
-func main() {
 	vessels := []*pb.Vessel{
 		&pb.Vessel{Id: "vessel01", Name: "Kane's Salty Secret", MaxWeight: 200000, Capacity: 500},
 	}
-	repo := &vesselRepository{vessels}
+	for _, v := range vessels {
+		repo.Create(v)
+	}
+}
+
+func main() {
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = defaultHost
+	}
+
+	session, err := CreateSession(host)
+	defer session.Close()
+	if err != nil {
+		log.Panicf("Unable to connect to the datastore with host %s - %v", host, err)
+	}
+
+	repo := &VesselRepository{session.Copy()}
+	createDummyData(repo)
 
 	srv := micro.NewService(
 		micro.Name("go.micro.srv.vessel"),
@@ -59,7 +48,7 @@ func main() {
 
 	srv.Init()
 
-	pb.RegisterVesselServiceHandler(srv.Server(), &service{repo})
+	pb.RegisterVesselServiceHandler(srv.Server(), &service{session})
 
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
